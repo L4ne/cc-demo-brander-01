3b3ed5cdd1cdd2a90cd082499ec880136984d304
Kieran Lane <klane@klane-ltm.internal.salesforce.com>
2018-08-30 15:10:29 +0100

    testing

--- server.js
+++ server.js
@@ -47,32 +47,32 @@ app.get('/', (req, res) => {
   });
 });
 
-// app.get('/css/global.css', (req, res) => {
-//   res.render('global.hbs',
-//    {
-//     ccdemo_Id:                  '00000001',
-//     ccdemo_name:                'Northern Trail Outfitters',
-//     ccdemo_colour_grey1:        '#f9f9f9',
-//     ccdemo_colour_grey2:        '#eeeeee',
-//     ccdemo_colour_grey3:        '#cccccc',
-//     ccdemo_colour_grey4:        '#999999',
-//     ccdemo_colour_grey5:        '#666666',
-//     ccdemo_colour_grey6:        '#444444',
-//     ccdemo_colour_grey7:        '#222222',
-//     ccdemo_colour_greytrans1:   'rgba(0,0,0,0.65)',
-//     ccdemo_colour_greytrans2:   'rgba(0,0,0,0.25)',
-//     ccdemo_colour_brandold:     '#59ba00',
-//     ccdemo_colour_brandnew:     '#00a1e0',
-//     ccdemo_colour_brandnewdark: '#0070d2',
-//     ccdemo_colour_success:      '#008827',
-//     ccdemo_colour_warning:      '#ffb75d',
-//     ccdemo_colour_danger:       '#cc0000',
+app.get('/css/global.css', (req, res) => {
+  res.render('global.hbs',
+   {
+    ccdemo_Id:                  '00000001',
+    ccdemo_name:                'Northern Trail Outfitters',
+    ccdemo_colour_grey1:        '#f9f9f9',
+    ccdemo_colour_grey2:        '#eeeeee',
+    ccdemo_colour_grey3:        '#cccccc',
+    ccdemo_colour_grey4:        '#999999',
+    ccdemo_colour_grey5:        '#666666',
+    ccdemo_colour_grey6:        '#444444',
+    ccdemo_colour_grey7:        '#222222',
+    ccdemo_colour_greytrans1:   'rgba(0,0,0,0.65)',
+    ccdemo_colour_greytrans2:   'rgba(0,0,0,0.25)',
+    ccdemo_colour_brandold:     '#59ba00',
+    ccdemo_colour_brandnew:     '#00a1e0',
+    ccdemo_colour_brandnewdark: '#0070d2',
+    ccdemo_colour_success:      '#008827',
+    ccdemo_colour_warning:      '#ffb75d',
+    ccdemo_colour_danger:       '#cc0000',
     
-//     pageTitle: 'Home Page',
-//     welcomeMessage: '#ffcc33',
-//     siteId: req.query.siteid
-//   });
-// });
+    pageTitle: 'Home Page',
+    welcomeMessage: '#ffcc33',
+    siteId: req.query.siteid
+  });
+});
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
